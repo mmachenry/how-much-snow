7629e8cac4a2750cfded15c536c5070cce6d06af
Mike MacHenry <mike.machenry@gmail.com>
2015-01-26 14:37:28 -0500

    oye

--- update_weather_data.py
+++ update_weather_data.py
@@ -44,7 +44,7 @@ def get_latest_run_filenames (ftp):
     ftp.cwd(latestDirectory)
     filenames = ftp.nlst()
     l = len(filenames)
-    return sorted(filenames)[l-30:l]
+    return sorted(filenames)[l-30:l-1]
 
 def convert_to_csv (temp_dir):
     DEVNULL = open(os.devnull, 'w')
