deb12a36be1ffe6694aa4e45cf130b4a4ad8b79c
Mike MacHenry <mike.machenry@gmail.com>
2018-04-01 20:34:40 -0400

    Elm prototype in preparation for move to single-page app as well as AWS move.

---
+++ web-app/src/Main.elm
@@ -0,0 +1,60 @@
+module Main exposing (main)
+
+import Html exposing (Html, p, text)
+-- import Html.Attributes exposing ()
+import Task
+import Geolocation
+import Json.Decode as Json
+import Http
+
+main =
+  Html.program {
+    init = init,
+    update = update,
+    subscriptions = subscriptions,
+    view = view
+  }
+
+type Model =
+    InitState
+  | ErrorState String
+  | LocationState Geolocation.Location
+  | Snow SnowResult
+
+type SnowResult = SnowResult String String
+
+type Msg =
+    UpdateLocation (Result Geolocation.Error Geolocation.Location)
+  | UpdateSnow (Result Http.Error SnowResult)
+
+init : (Model, Cmd Msg)
+init = (InitState, Task.attempt UpdateLocation Geolocation.now)
+
+update : Msg -> Model -> (Model, Cmd Msg)
+update msg model = case msg of
+  UpdateLocation (Ok loc) -> (model, getSnow loc)
+  UpdateLocation (Err err) -> (ErrorState (toString err), Cmd.none)
+  UpdateSnow (Ok x) -> (Snow x, Cmd.none)
+  UpdateSnow (Err err) -> (ErrorState (toString err), Cmd.none)
+
+subscriptions : Model -> Sub Msg
+subscriptions model = Sub.none
+
+view : Model -> Html Msg
+view model = case model of
+  InitState -> p [] [ text "Getting location..." ]
+  ErrorState err -> p [] [ text (toString err) ]
+  LocationState loc -> p [] [ text (toString loc) ]
+  Snow sr -> p [] [ text (toString sr) ]
+
+getSnow : Geolocation.Location -> Cmd Msg
+getSnow loc =
+  let url = "https://howmuchsnowamigoingtoget.com/?geo=1&lat="
+            ++ toString loc.latitude ++ "&lon=" ++ toString loc.longitude
+  in Http.send UpdateSnow (Http.get url decodeSnow)
+
+decodeSnow : Json.Decoder SnowResult
+decodeSnow =
+  Json.map2 SnowResult
+    (Json.field "coords" Json.string)
+    (Json.field "inches" Json.string)
