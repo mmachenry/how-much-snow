a234ef2903a7691c570c97192af834dd35c92cc1
Mike MacHenry <mike.machenry@gmail.com>
2021-02-19 12:36:37 -0500

    Be more generous with the use of the word 'more' in the output

--- front-end/src/Main.elm
+++ front-end/src/Main.elm
@@ -179,8 +179,7 @@ isCurrentlySnowing : List (PredictionDatum, Float, Float) -> Bool
 isCurrentlySnowing data =
   List.any
     (\(p, _, timeLeft)->
-         p.metersofsnow > 0.01
-      && timeLeft > 0
+         p.metersofsnow > 0.005
       && timeLeft < 1)
     data
 
